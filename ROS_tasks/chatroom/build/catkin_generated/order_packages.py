9adb4a6918e23564c847b0424abbaaf8b35c9136
Yunjay-singh <160787133+Yunjay-singh@users.noreply.github.com>
2024-04-10 20:48:28 +0530

    Delete ROS_tasks (wrong ) directory

--- ROS_tasks/chatroom/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mrityunjaya/ROS_tasks/chatroom/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/mrityunjaya/ROS_tasks/chatroom/devel;/opt/ros/noetic'.split(';') if '/home/mrityunjaya/ROS_tasks/chatroom/devel;/opt/ros/noetic' != '' else []
--- ROS_tasks/chatroom/build/chatroom/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mrityunjaya/ROS_tasks/chatroom/src/chatroom
-# Build directory: /home/mrityunjaya/ROS_tasks/chatroom/build/chatroom
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
